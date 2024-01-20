cc56457a722a414c9009d64c5350602892701857
asapoka <asapoka@outlook.com>
2024-01-21 01:04:31 +0900

    mod add mkdir

--- setup.bash
+++ setup.bash
@@ -1,4 +1,5 @@
 wget https://dw.hanbit.co.kr/Oracle/11gXE/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
+mkdir 11.2.0.2
 mv oracle-xe-11.2.0-1.0.x86_64.rpm.zip 11.2.0.2
 git clone https://github.com/oracle/docker-images.git
 cp -r docker-images/OracleDatabase/SingleInstance/dockerfiles/11.2.0.2 .
