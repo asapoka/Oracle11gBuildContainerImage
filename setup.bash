d0f14f73aa4b1f1d68f32a6788534de8829649f5
asapoka <asapoka@outlook.com>
2024-01-24 22:50:23 +0900

    add alter link

--- setup.bash
+++ setup.bash
@@ -1,4 +1,5 @@
 wget https://dw.hanbit.co.kr/Oracle/11gXE/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
+# wget https://www.iea-software.com/ftp/emeraldv5/linux/ora/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
 mkdir 11.2.0.2
 mv oracle-xe-11.2.0-1.0.x86_64.rpm.zip 11.2.0.2
 git clone https://github.com/oracle/docker-images.git
