e16b32c86e8b3efa39b64657cb0745ce752b8850
asapoka <asapoka@outlook.com>
2024-01-30 23:37:12 +0900

    mod

--- setup.bash
+++ setup.bash
@@ -5,6 +5,7 @@ if [ -f ./oracle-xe-11.2.0-1.0.x86_64.rpm.zip ]; then
     mv oracle-xe-11.2.0-1.0.x86_64.rpm.zip 11.2.0.2
 else
     echo "Please put oracle-xe-11.2.0-1.0.x86_64.rpm.zip current directory"
+    exit 1
     # wget https://dw.hanbit.co.kr/Oracle/11gXE/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
     # wget https://www.iea-software.com/ftp/emeraldv5/linux/ora/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
 fi
